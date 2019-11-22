1143e21e17eeecfed85ccee66e7908f913175c81
Eugen Druta <edruta@gmx.de>
2019-11-22 21:26:57 +0100

    Cleaning

--- globalconfig.js
+++ globalconfig.js
@@ -1,8 +1,7 @@
 //Initialisierung Dialoge
-//Initialisierung Dialoge
 var globalconfig = {
-  "LOGGER": true,
-  "feserver": "localhost:8090",
-  "bckendurl": "http://localhost:8080/WebRWinMvn/",
-  "startdialog": "avueb" //"bsueb"
+    "LOGGER": true,
+    "feserver": "localhost:8090",
+    "bckendurl": "http://localhost:8080/WebRWinMvn/",
+    "startdialog": "avueb" //"bsueb"
 };
